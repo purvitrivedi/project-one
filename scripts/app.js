c0df5dc532edf09c4c83bf03208fda3c54438754
purvitrivedi <purvitriv@gmail.com>
2020-04-10 20:46:42 +0100

    Oshape is now falling

--- scripts/app.js
+++ scripts/app.js
@@ -16,76 +16,16 @@ function init() {
 
   let pos = 5
 
-  const oShape = [pos, pos + 1, pos + width, pos + width + 1]
-  const iShape = [pos, pos - 1, pos - 2, pos + 1]
-  const sShape = [pos, pos + 1, pos + width, pos + width - 1]
-  const zShape = [pos, pos - 1, pos + width, pos + width + 1]
-  const lShape = [pos, pos - 1 + width, pos - 1, pos + 1]
-  const jShape = [pos, pos - 1, pos + 1, pos + 1 + width]
-  const tShape = [pos, pos + width, pos - 1, pos + 1]
 
 
-  //*Functions
 
 
-  function createCells() {
-    for (let i = 0; i < cellCount; i++) {
-      const cell = document.createElement('div')
-      grid.appendChild(cell)
-      cell.textContent = i
-      cells.push(cell)
-      if (i % 2 !== 0) {
-        cell.classList.add('odd')
-      }
-    }
-  }
+
+
+
+
 
   createCells()
-
-
-
-  function tetriminoFalling() {
-
-    // const x = tetriminoPosition % width
-    // const y = Math.floor(tetriminoPosition / width)
-    // console.log(y)
-
-    const timerId = setInterval(() => {
-      cells[pos].classList.remove('gold-shape')
-      pos += width
-      cells[pos].classList.add('gold-shape')
-
-
-      if (pos > 227) clearInterval(timerId)
-
-    }, 500)
-  }
-
-  // tetriminoFalling()
-
-  function moveTetriminos(event) {
-    cells[pos].classList.remove('gold-shape')
-
-    switch (event.keyCode) {
-      case 39:
-        console.log('should move right')
-        pos++
-        break
-      case 37:
-        console.log('should move left')
-        pos--
-        break
-      default:
-        console.log('invalid key')
-    }
-
-    cells[pos].classList.add('gold-shape')
-  }
-
-  // * Event Listeners
-
-  document.addEventListener('keyup', moveTetriminos)
-
 
 
   // * Tetriminos
@@ -268,23 +208,107 @@ function init() {
   }
 
 
+  let oShape = [pos, pos + 1, pos + width, pos + width + 1]
+  const iShape = [pos, pos - 1, pos - 2, pos + 1]
+  const sShape = [pos, pos + 1, pos + width, pos + width - 1]
+  const zShape = [pos, pos - 1, pos + width, pos + width + 1]
+  const lShape = [pos, pos - 1 + width, pos - 1, pos + 1]
+  const jShape = [pos, pos - 1, pos + 1, pos + 1 + width]
+  const tShape = [pos, pos + width, pos - 1, pos + 1]
 
-  const o = new Tetrimino('o', oShape, 'o-shape')
+
+
+  let o = new Tetrimino('o', oShape, 'o-shape')
   const i = new I('i', iShape, 'i-shape')
   const s = new S('s', sShape, 's-shape')
   const z = new Z('z', zShape, 'z-shape')
   const l = new L('l', lShape, 'l-shape')
   const j = new J('j', jShape, 'j-shape')
   const t = new T('t', tShape, 't-shape')
-  t.createShape()
-  t.rotateFirst()
-  t.rotateSecond()
-  t.rotateThird()
-  t.rotateFinal()
+
+ 
+  // o.createShape()
+
+  // cells[pos].classList.add('gold-shape')
 
 
 
-  cells[pos].classList.add('gold-shape')
+
+
+
+
+
+
+
+
+
+  //*Functions
+
+  function createCells() {
+    for (let i = 0; i < cellCount; i++) {
+      const cell = document.createElement('div')
+      grid.appendChild(cell)
+      // cell.textContent = i
+      cells.push(cell)
+      if (i % 2 !== 0) {
+        cell.classList.add('odd')
+      }
+    }
+  }
+
+
+  function tetriminoFalling() {
+    o.createShape()
+
+    const timerId = setInterval(() => {
+      o.removeShape()
+      oShape = oShape.map(cell => {
+        return cell = cell + width
+      })
+      o = new Tetrimino('o', oShape, 'o-shape')
+      console.log(oShape)
+      o.createShape()
+      // cells[pos].classList.add('gold-shape')
+
+
+      if (Math.max(...oShape) > 227) clearInterval(timerId)
+
+    }, 500)
+  }
+
+  tetriminoFalling()
+
+
+
+  // const myArray = [1,2,3,4]
+  // console.log(Math.max(...myArray))
+
+  function moveTetriminos(event) {
+    cells[pos].classList.remove('gold-shape')
+
+    switch (event.keyCode) {
+      case 39:
+        console.log('should move right')
+        pos++
+        break
+      case 37:
+        console.log('should move left')
+        pos--
+        break
+      default:
+        console.log('invalid key')
+    }
+
+    // cells[pos].classList.add('gold-shape')
+  }
+
+  // * Event Listeners
+
+  // document.addEventListener('keyup', moveTetriminos)
+
+
+
+
 
 
 }
