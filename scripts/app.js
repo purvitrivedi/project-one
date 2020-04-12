565933b457d148fa2aea95debf7fd9b70cfd8dc1
purvitrivedi <purvitriv@gmail.com>
2020-04-12 13:15:47 +0100

    ALL ROTATIONS WORK! Next step: get sshapes to stay at the bottom

--- scripts/app.js
+++ scripts/app.js
@@ -210,31 +210,38 @@ function init() {
     constructor(name, dimensions, className) {
       super(name, dimensions, className)
     }
-    rotateFirst() {
-      this.removeShape()
-      pos = pos + width
-      this.dimensions = [pos, pos - width, pos + width, pos - width + 1]
-      this.createShape()
-    }
 
-    rotateSecond() {
-      this.removeShape()
-      this.dimensions = [pos, pos - 1, pos + 1, pos - width - 1]
-      this.createShape()
-    }
+    rotateJ() {
+      if (rotationNum === 0) {
+        this.removeShape()
+        this.dimensions = this.dimensions.map((cell, index) => {
+          return cell += jRotateFirst[index]
+        })
+        this.createShape()
+        rotationNum++
 
-
-    rotateThird() {
-      this.removeShape()
-      this.dimensions = [pos, pos - width, pos + width, pos + width - 1]
-      this.createShape()
-    }
-
-    rotateFinal() {
-      pos = pos - width
-      this.removeShape()
-      this.dimensions = jShape
-      this.createShape()
+      } else if (rotationNum === 1) {
+        this.removeShape()
+        this.dimensions = this.dimensions.map((cell, index) => {
+          return cell += jRotateSecond[index]
+        })
+        this.createShape()
+        rotationNum++
+      } else if (rotationNum === 2) {
+        this.removeShape()
+        this.dimensions = this.dimensions.map((cell, index) => {
+          return cell += jRotateThird[index]
+        })
+        this.createShape()
+        rotationNum++
+      } else {
+        this.removeShape()
+        this.dimensions = this.dimensions.map((cell, index) => {
+          return cell += jRotateLast[index]
+        })
+        this.createShape()
+        rotationNum = 0
+      }
     }
   }
 
@@ -242,33 +249,37 @@ function init() {
     constructor(name, dimensions, className) {
       super(name, dimensions, className)
     }
+    rotateT() {
+      if (rotationNum === 0) {
+        this.removeShape()
+        this.dimensions = this.dimensions.map((cell, index) => {
+          return cell += tRotateFirst[index]
+        })
+        this.createShape()
+        rotationNum++
 
-
-    rotateFirst() {
-      this.removeShape()
-      pos = pos + width
-      this.dimensions = [pos, pos - width, pos + width, pos + 1]
-      this.createShape()
-    }
-
-    rotateSecond() {
-      this.removeShape()
-      this.dimensions = [pos, pos - width, pos + 1, pos - 1]
-      this.createShape()
-    }
-
-
-    rotateThird() {
-      this.removeShape()
-      this.dimensions = [pos, pos - width, pos + width, pos - 1]
-      this.createShape()
-    }
-
-    rotateFinal() {
-      pos = pos - width
-      this.removeShape()
-      this.dimensions = tShape
-      this.createShape()
+      } else if (rotationNum === 1) {
+        this.removeShape()
+        this.dimensions = this.dimensions.map((cell, index) => {
+          return cell += tRotateSecond[index]
+        })
+        this.createShape()
+        rotationNum++
+      } else if (rotationNum === 2) {
+        this.removeShape()
+        this.dimensions = this.dimensions.map((cell, index) => {
+          return cell += tRotateThird[index]
+        })
+        this.createShape()
+        rotationNum++
+      } else {
+        this.removeShape()
+        this.dimensions = this.dimensions.map((cell, index) => {
+          return cell += tRotateLast[index]
+        })
+        this.createShape()
+        rotationNum = 0
+      }
     }
   }
 
@@ -293,8 +304,19 @@ function init() {
   const lRotateLast = [0, -11, 11, 24]
 
 
-  const jShape = [pos, pos - 1, pos + 1, pos + 1 + width]
-  const tShape = [pos, pos + width, pos - 1, pos + 1]
+  const jShape = [5, 4, 6, 18]
+  const jRotateFirst = [0, 13, -13, -24]
+  const jRotateSecond = [0, -11, 11, -2]
+  const jRotateThird = [0, -13, 13, 24]
+  const jRotateLast = [0, 11, -11, 2]
+
+
+  const tShape = [5, 6, 4, 17]
+  const tRotateFirst = [0, -13, 13, -11]
+  const tRotateSecond = [0, 11, -11, -13]
+  const tRotateThird = [0, 13, -13, 11]
+  const tRotateLast = [0, -11, 11, 13]
+
 
   const o = new Tetrimino('o', oShape, 'o-shape')
   const i = new I('i', iShape, 'i-shape')
@@ -304,7 +326,6 @@ function init() {
   const j = new J('j', jShape, 'j-shape')
   const t = new T('t', tShape, 't-shape')
 
-  const myArray = [l.name]
 
 
   //*Functions
@@ -321,7 +342,7 @@ function init() {
     }
   }
 
-  // const myArray = [o.name, i.name, s.name, z.name, l.name, j.name, t.name]
+  const myArray = [o.name, i.name, s.name, z.name, l.name, j.name, t.name]
 
 
   function generateTetrimino() {
