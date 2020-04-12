7ae24d801d152d235421cbdc904bbedc8a1a9b69
purvitrivedi <purvitriv@gmail.com>
2020-04-12 12:49:08 +0100

    fixing rotations for all shapes

--- scripts/app.js
+++ scripts/app.js
@@ -13,8 +13,9 @@ function init() {
 
 
   // * Game Variables
-
   let pos = 5
+  let rotationNum = 0
+
 
 
   // * Create Grid
@@ -47,11 +48,9 @@ function init() {
       this.createShape()
       const timerId = setInterval(() => {
         this.removeShape()
-        // pos = pos + width
         this.dimensions = this.dimensions.map(cell => {
           return cell += width
         })
-        console.log(pos)
         this.createShape()
         if (Math.max(...this.dimensions) > 227) clearInterval(timerId)
       }, 500)
@@ -65,7 +64,6 @@ function init() {
           this.dimensions = this.dimensions.map(cell => {
             return cell += 1
           })
-          console.log(pos)
           break
         case 37:
           this.removeShape()
@@ -90,6 +88,7 @@ function init() {
 
   }
 
+
   class I extends Tetrimino {
     constructor(name, dimensions, className) {
       super(name, dimensions, className)
@@ -97,17 +96,17 @@ function init() {
     rotateI() {
       if (this.rotateI.called) {
         this.removeShape()
-        for (let i = 0; i < this.dimensions.length; i++) {
-          this.dimensions[i] -= iRotate[i]
-        }
+        this.dimensions = this.dimensions.map((cell, index) => {
+          return cell -= iRotate[index]
+        })
         this.createShape()
         console.log(pos)
         this.rotateI.called = false
       } else {
         this.removeShape()
-        for (let i = 0; i < this.dimensions.length; i++) {
-          this.dimensions[i] += iRotate[i]
-        }
+        this.dimensions = this.dimensions.map((cell, index) => {
+          return cell += iRotate[index]
+        })
         this.createShape()
         this.rotateI.called = true
       }
@@ -123,17 +122,17 @@ function init() {
     rotateS() {
       if (this.rotateS.called) {
         this.removeShape()
-        for (let i = 0; i < this.dimensions.length; i++) {
-          this.dimensions[i] -= sRotate[i]
-        }
+        this.dimensions = this.dimensions.map((cell, index) => {
+          return cell -= sRotate[index]
+        })
         this.createShape()
         this.rotateS.called = false
       } else {
         this.removeShape()
         pos = pos + width
-        for (let i = 0; i < this.dimensions.length; i++) {
-          this.dimensions[i] += sRotate[i]
-        }
+        this.dimensions = this.dimensions.map((cell, index) => {
+          return cell += sRotate[index]
+        })
         this.createShape()
         this.rotateS.called = true
       }
@@ -149,17 +148,16 @@ function init() {
     rotateZ() {
       if (this.rotateZ.called) {
         this.removeShape()
-        for (let i = 0; i < this.dimensions.length; i++) {
-          this.dimensions[i] -= zRotate[i]
-        }
+        this.dimensions = this.dimensions.map((cell, index) => {
+          return cell -= zRotate[index]
+        })
         this.createShape()
         this.rotateZ.called = false
       } else {
         this.removeShape()
-        pos = pos + width
-        for (let i = 0; i < this.dimensions.length; i++) {
-          this.dimensions[i] += zRotate[i]
-        }
+        this.dimensions = this.dimensions.map((cell, index) => {
+          return cell += zRotate[index]
+        })
         this.createShape()
         this.rotateZ.called = true
       }
@@ -172,32 +170,40 @@ function init() {
       super(name, dimensions, className)
     }
 
-    rotateFirst() {
-      this.removeShape()
-      pos = pos + width
-      this.dimensions = [pos, pos - width, pos + width, pos + 1 + width]
-      this.createShape()
-    }
+    rotateL() {
+      if (rotationNum === 0) {
+        this.removeShape()
+        this.dimensions = this.dimensions.map((cell, index) => {
+          return cell += lRotateFirst[index]
+        })
+        this.createShape()
+        rotationNum++
 
-    rotateSecond() {
-      this.removeShape()
-      this.dimensions = [pos, pos - 1, pos + 1, pos - width + 1]
-      this.createShape()
-    }
-
-    rotateThird() {
-      this.removeShape()
-      this.dimensions = [pos, pos - width, pos + width, pos - width - 1]
-      this.createShape()
-    }
-
-    rotateFinal() {
-      pos = pos - width
-      this.removeShape()
-      this.dimensions = lShape
-      this.createShape()
+      } else if (rotationNum === 1) {
+        this.removeShape()
+        this.dimensions = this.dimensions.map((cell, index) => {
+          return cell += lRotateSecond[index]
+        })
+        this.createShape()
+        rotationNum++
+      } else if (rotationNum === 2) {
+        this.removeShape()
+        this.dimensions = this.dimensions.map((cell, index) => {
+          return cell += lRotateThird[index]
+        })
+        this.createShape()
+        rotationNum++
+      } else {
+        this.removeShape()
+        this.dimensions = this.dimensions.map((cell, index) => {
+          return cell += lRotateLast[index]
+        })
+        this.createShape()
+        rotationNum = 0
+      }
     }
   }
+
 
 
   class J extends Tetrimino {
@@ -280,9 +286,13 @@ function init() {
   const zRotate = [13, 0, -11, -24]
 
 
+  const lShape = [5, 6, 4, 16]
+  const lRotateFirst = [0, -13, 13, 2]
+  const lRotateSecond = [0, 11, -11, -24]
+  const lRotateThird = [0, 13, -13, -2]
+  const lRotateLast = [0, -11, 11, 24]
 
 
-  const lShape = [pos, pos - 1 + width, pos - 1, pos + 1]
   const jShape = [pos, pos - 1, pos + 1, pos + 1 + width]
   const tShape = [pos, pos + width, pos - 1, pos + 1]
 
@@ -294,6 +304,7 @@ function init() {
   const j = new J('j', jShape, 'j-shape')
   const t = new T('t', tShape, 't-shape')
 
+  const myArray = [l.name]
 
 
   //*Functions
@@ -311,7 +322,7 @@ function init() {
   }
 
   // const myArray = [o.name, i.name, s.name, z.name, l.name, j.name, t.name]
-  const myArray = [z.name]
+
 
   function generateTetrimino() {
     const newShape = myArray[Math.round(Math.random() * (myArray.length - 1))]
@@ -353,8 +364,8 @@ function init() {
           i.rotateI()
           break
         case 's':
-          break
           s.rotateS()
+          break
         case 'z':
           z.rotateZ()
           break
